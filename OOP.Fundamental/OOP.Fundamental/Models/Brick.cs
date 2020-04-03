72741039c941423ad036bb94b98745f211a56747
Đông Lương <d.vinhluong@ext.tricentis.com>
2020-04-03 22:56:22 +0700

    Brick.cs

---
+++ OOP.Fundamental/OOP.Fundamental/Models/Brick.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Fundamental.Models
+{
+    public class Brick
+    {
+        public string Name { get; private set; }
+        public double Length { get; private set; }
+        public double Height { get; private set; }
+        public double Width { get; private set; }
+
+        private double _surfaceArea 
+        {
+            get
+            {
+                return 2 * this.Length * this.Height +
+                       2 * this.Length * this.Width +
+                       2 * this.Height * this.Width;
+            }
+        }
+
+        public Brick(string name, double length, double height, double width)
+        {
+            this.Name = name;
+            this.Length = length;
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public double GetArea(double length, double height, double width)
+        {
+            return _surfaceArea;
+        }
+    }
+}
